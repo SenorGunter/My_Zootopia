e91a28c038c6fc4f729c1cf79c5a69247e407d80
SenorGunter <163559186+SenorGunter@users.noreply.github.com>
2024-12-03 14:18:20 +0100

    Commiting everything from local

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,9 +2,30 @@ import json
 
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads JSON"""
+    with open(file_path, "r") as fileobj:
+        return json.load(fileobj)
+
+
+def load_html_template(file_path):
+    """ Loads HTML file """
+    with open(file_path, "r") as fileobj:
+        return fileobj.read()
+
+
+def get_animals_data(skin_type):
+    """ Returns a dictionary with the relevant data for each animal """
+    full_animals_data = load_data("animals_data.json")
+
+    animals_data = {}
+    for animal in full_animals_data:
+        try:
+            animal_skin_type = animal["characteristics"]["skin_type"]
+        except KeyError:
+            animal_skin_type = "not specified"
+
+        if skin_type != animal_skin_type and skin_type != "All":
+            continue
 
 
 def printing_all_names(data):
